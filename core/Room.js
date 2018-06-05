e4439be521a528141e84c3b53baa3a84020790d5
Takashiro <takashiro@qq.com>
2018-06-05 20:26:37 +0800

    Add class Room

---
+++ core/Room.js
@@ -0,0 +1,133 @@
+
+const EventEmitter = require('events');
+const net = require('./protocol');
+
+let NEXT_ROOM_ID = 0;
+
+class Room extends EventEmitter {
+
+	/**
+	 * Create a new instance of Room
+	 * @param {User} owner the room owner
+	 */
+	constructor(owner) {
+		super();
+
+		this.id = 0;
+		this.owner = owner;
+		this.driver = null;
+
+		this.users = new Set;
+		this.addUser(owner);
+	}
+
+	/**
+	 * Find a user by user id
+	 * @param {number} id user id
+	 */
+	findUser(id) {
+		for (let user of this.users) {
+			if (user.id === id) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Add a user into this room
+	 * @param {User} user
+	 */
+	addUser(user) {
+		if (user.room) {
+			user.room.removeUser(user);
+		}
+		user.room = this;
+		this.users.add(user);
+
+		user.on('close', () => this.removeUser(user));
+	}
+
+	/**
+	 * Remove a user from this room
+	 * @param {User} user
+	 */
+	removeUser(user) {
+		user.room = null;
+		this.users.delete(user.id);
+
+		if (this.users.size <= 0) {
+			this.emit('close');
+		}
+	}
+
+	/**
+	 * Broadcast a command to all users in this room
+	 * @param {number} command
+	 * @param {object} args
+	 */
+	broadcast(command, args = null) {
+		for (let user of this.users) {
+			user.send(command, args);
+		}
+	}
+
+	/**
+	 * Broadcast a notification to all users in this room
+	 * @param {number} command
+	 * @param {object} args
+	 */
+	broadcastNotification(command, args) {
+		this.broadcast(net.Notify, {
+			cmd: command,
+			arg: args,
+		});
+	}
+
+	/**
+	 * Getter of room configuration
+	 */
+	getConfig() {
+		let config = {
+			id: this.id,
+			owner: {
+				id: this.owner.id
+			},
+			driver: null
+		};
+
+		if (this.driver && this.driver.getConfig) {
+			config.driver = this.driver.getConfig();
+			config.driver.name = this.driver.name;
+		}
+	}
+
+	/**
+	 * Setter of room configuration
+	 * @param {*} config
+	 */
+	updateConfig(config) {
+		if (this.driver && this.driver.setConfig) {
+			this.driver.setConfig(config);
+		}
+	}
+
+	/**
+	 * Load game extension
+	 * @param {string} name driver name
+	 * @return {boolean}
+	 */
+	loadExtension(name) {
+		try {
+			const GameDriver = require('../extension/' + name);
+			this.driver = new GameDriver(this);
+			return true;
+		} catch (error) {
+			console.log(error.stack);
+			return false;
+		}
+	}
+
+}
+
+module.exports = Room;
