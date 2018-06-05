a9928f311676a25ca10bfb8836b741daa1f87d5a
Takashiro <takashiro@qq.com>
2018-06-05 20:28:56 +0800

    Add class Server

---
+++ core/Server.js
@@ -0,0 +1,74 @@
+
+class Server {
+
+	/**
+	 * Create a new instance of Server
+	 */
+	constructor() {
+		this.users = new Map;
+		this.nextUserId = 0;
+		this.rooms = new Map;
+		this.nextRoomId = 0;
+	}
+
+	/**
+	 * Add a new room
+	 * @param {Room} room
+	 */
+	addRoom(room) {
+		this.nextRoomId++;
+		room.id = this.nextRoomId;
+		this.rooms.set(room.id, room);
+
+		room.on('close', () => this.removeRoom(room.id));
+	}
+
+	/**
+	 * Find a room by room id
+	 * @param {number} id room id
+	 */
+	findRoom(id) {
+		return this.rooms.get(id);
+	}
+
+	/**
+	 * Remove a room by room id
+	 * @param {number} id room id
+	 */
+	removeRoom(id) {
+		this.rooms.delete(id);
+	}
+
+	/**
+	 * Add a new connected user and assign user id
+	 * @param {User} user the user instance
+	 */
+	addUser(user) {
+		this.nextUserId++;
+		user.id = this.nextUserId;
+		user.server = this;
+
+		this.users.set(user.id, user);
+		user.on('close', () => this.removeUser(user.id));
+	}
+
+	/**
+	 * Find a user by user id
+	 * @param {number} id user id
+	 * @return {User}
+	 */
+	findUser(id) {
+		return this.users.get(id);
+	}
+
+	/**
+	 * Remove a user by user id
+	 * @param {number} id user id
+	 */
+	removeUser(id) {
+		this.users.delete(id);
+	}
+
+}
+
+module.exports = Server;
