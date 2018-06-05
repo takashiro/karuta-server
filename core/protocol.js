b65049db6395f6e701bd1df1498ae0e0c5917eab
Takashiro <takashiro@qq.com>
2018-06-05 23:29:31 +0800

    Add system commands

---
+++ core/protocol.js
@@ -0,0 +1,29 @@
+
+const net = {
+	Invalid: 0x0,
+
+	CheckVersion: 0x1,
+
+	Login: 0x2,
+	Logout: 0x3,
+
+	CreateRoom: 0x4,
+	EnterRoom: 0x5,
+	LeaveRoom: 0x6,
+	UpdateRoom: 0x7,
+
+	SetUserList: 0x8,
+	AddUser: 0x9,
+	RemoveUser: 0xA,
+
+	Speak: 0xB,
+
+	LoadGame: 0xC,
+	Notify: 0xD,
+	Request: 0xE,
+	Reply: 0xF,
+
+	NetworkCommandCount: 0x10,
+};
+
+module.exports = net;
