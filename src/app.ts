00c8904127774f5baa89fcfc4e63f5ef605dec18
Kazuichi Takashiro <takashiro@qq.com>
2020-01-10 21:06:06 +0800

    Fix eslint errors of main()

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import App from './core/App';
 import readConfig from './util/readConfig';
 
 // Start up application
-(async function () {
+(async function main(): Promise<void> {
 	const config = await readConfig();
 	const app = new App(config);
 	await app.start();
