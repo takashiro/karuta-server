62b944e2fa0cff8a9833c635913d3ba199b4b301
Kazuichi Takashiro <takashiro@qq.com>
2020-01-11 16:16:49 +0800

    Update the definition of Driver

--- src/core/Driver.ts
+++ src/core/Driver.ts
@@ -1,10 +1,13 @@
-import Action from './Action';
+import Action from '../net/Action';
 
 interface Driver {
 	getName(): string;
+
 	setConfig(config: object): void;
 	getConfig(): object;
-	getAction(command: number): Action;
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	getAction(command: number): Action<any, any>;
 }
 
 export default Driver;
