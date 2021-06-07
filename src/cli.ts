bfc9667fad7f29693f19d3d755e2c87b964439b1
Kazuichi Takashiro <takashiro@qq.com>
2021-06-07 21:42:06 +0800

    Add cli shebang

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,8 @@
+#!/usr/bin/env node
+
 import App from './core/App';
 import readConfig from './util/readConfig';
 
-// Start up application
 (async function main(): Promise<void> {
 	const config = await readConfig();
 	const app = new App(config);
